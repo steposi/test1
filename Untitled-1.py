2a77b0b2644dcf1cd30c37da97d96565c03cc7a2
steposi <steposimus@gmail.com>
2019-09-05 23:37:54 +0300

    4

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,6 +3,3 @@ print(math.cos(0))
 print("1")
 print(math.sin(90))
 print("Hello world")
-a = 84
-
-c = 67
--- test1/Untitled-1.py
+++
@@ -1,7 +0,0 @@
-import math
-print(math.cos(0))
-print("1")
-print(math.sin(90))
-print("Hello world")
-a = 84
-b = 56
